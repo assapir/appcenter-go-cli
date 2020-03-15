4c482a63f809aa82c0be4583846c195c40c12c65
Assaf Sapir <assapir@microsoft.com>
2020-03-15 14:36:37 +0200

    upload file

--- appcenter.go
+++ appcenter.go
@@ -1,51 +1,14 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/assapir/appcenter-go-cli/config"
+	"github.com/assapir/appcenter-go-cli/httpclient"
 )
-
-func post(addr string, body interface{}, apiToken string, jsonBody bool) ([]byte, error) {
-	var bodyValue []byte
-	if jsonBody {
-		bodyValue, _ = json.Marshal(body)
-	}
-
-	req, err := http.NewRequest("POST", addr, bytes.NewBuffer(bodyValue))
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Accept", "application/json")
-	req.Header.Set("X-API-Token", apiToken)
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode < http.StatusOK || resp.StatusCode >= http.StatusBadRequest {
-		return nil, fmt.Errorf("Error while calling %s. Status: %s, body: %s", addr, resp.Status, string(bodyBytes))
-	}
-
-	log.Println("response status:", resp.Status)
-
-	return bodyBytes, nil
-}
 
 func createUpload(config *config.Config) (uploadID string, uploadURL string, err error) {
 	const baseURL = "https://appcenter.ms/api/v0.1/apps/%s/%s/release_uploads"
@@ -53,7 +16,7 @@ func createUpload(config *config.Config) (uploadID string, uploadURL string, err
 	log.Println(createUploadURL)
 
 	var body struct{}
-	jsonBytes, err := post(createUploadURL, &body, config.APIKey, true)
+	jsonBytes, err := httpclient.PostJSON(createUploadURL, &body, config.APIKey)
 	if err != nil {
 		return
 	}
@@ -68,20 +31,25 @@ func createUpload(config *config.Config) (uploadID string, uploadURL string, err
 }
 
 func main() {
-	args := os.Args[1:]
-	if len(args) == 0 {
+	arg := os.Args[1]
+	if len(arg) == 0 {
 		log.Fatal("error: no command line arguments supplied")
 	}
 
-	log.Println("got with this args:", args)
+	log.Println("got with this arg:", arg)
 
 	config, err := config.GetConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	_, _, err2 := createUpload(config)
-	if err2 != nil {
+	_, uploadURL, err := createUpload(config)
+	if err != nil {
 		log.Fatal(err)
+	}
+
+	_, err2 := httpclient.PostFile(uploadURL, arg)
+	if err2 != nil {
+		log.Fatal(err2)
 	}
 }
